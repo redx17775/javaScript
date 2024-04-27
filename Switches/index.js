499ee7ee7af84608e760c83c9f1968ca0936dbe8
redx17775 <65367038+redx17775@users.noreply.github.com>
2024-04-27 21:47:10 +0400

    Switches
    i finish it :)

---
+++ Switches/index.js
@@ -0,0 +1,28 @@
+let day = 10;
+
+switch (day) {
+  case 1:
+    console.log("it's Sunday");
+    break;
+  case 2:
+    console.log("it's Monday");
+    break;
+  case 3:
+    console.log("it's Tuesday");
+    break;
+  case 4:
+    console.log("it's Wendesday");
+    break;
+  case 5:
+    console.log("it's Thursday");
+    break;
+  case 6:
+    console.log("it's Friday");
+    break;
+  case 7:
+    console.log("it's Saturday");
+    break;
+  default:
+    console.log(`${day} not a day`);
+    break;
+}
